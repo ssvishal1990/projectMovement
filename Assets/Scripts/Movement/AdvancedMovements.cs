b18ae106850ea0ac1b2599194dfbc79256e9abcc
ssvishal1990 <ssvishal1990@gmail.com>
2022-03-23 12:07:37 +0530

    Adding movement through force

--- Assets/Scripts/Movement/AdvancedMovements.cs
+++ Assets/Scripts/Movement/AdvancedMovements.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 public class AdvancedMovements : MonoBehaviour
 {
 
@@ -11,11 +12,32 @@ public class AdvancedMovements : MonoBehaviour
     [SerializeField] private SpriteRenderer playerSprite;
 
 
-    [Header("Movement values")]
+    [Header("Movement values through velocity")]
     [SerializeField] private float movementAcceleration;
     [SerializeField] private float _maxspeed;
     [SerializeField] float _linearDrag;
-    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float jForce = 5f;
+
+    [Header("Movement components through force")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deacceleration;
+    [SerializeField] private float velocityPower;
+    [SerializeField] private float frictionAmount;
+
+
+    [Header("Jumping through force")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] [Range(0, 1)] public float jumpCutMultiplier;
+    [SerializeField] private float jumpCoyoteTime;
+    [SerializeField] private float jumpBufferTime;
+    [SerializeField] private float fallGravityMultiplier;
+
+
+    [Header("Checks for ground")]
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private Vector2 GroundCheckBoxSize;
+
 
 
     [Header("Layer Details")]
@@ -39,7 +61,8 @@ public class AdvancedMovements : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        moveThroughForce();
+        // move();
         applyFriction();
     }
 
@@ -69,6 +92,22 @@ public class AdvancedMovements : MonoBehaviour
         checkAndFlip();
     }
 
+
+    private async void moveThroughForce(){
+        float targetSpeed = horizontalDirection * moveSpeed;
+        float speedDif = targetSpeed - playerRb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deacceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velocityPower) * Mathf.Sign(speedDif);
+        Debug.Log("Still applying movement ++  " + movement  + " accel rate being  " + accelRate + "  targetSpeed being" + targetSpeed + " speedDifference beging" + speedDif);
+        Debug.Log("Velocity power statement  ++ " + Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velocityPower));
+
+        if(!Mathf.Approximately(movement, 0.0f)){    
+            playerRb.AddForce(movement * Vector2.right);
+        }
+        
+        checkAndFlip();
+    }
+
     private void checkAndFlip()
     {
         if (Mathf.Sign(playerRb.velocity.x) < 0 && facingRight)
@@ -94,7 +133,7 @@ public class AdvancedMovements : MonoBehaviour
     {
         if (context.performed && playerRb.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
-            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jForce);
         }
 
         // if (context.canceled && playerRb.velocity.y > 0f)
